ac108d7167cbf8bc8a08038973b7a1f261bc66f9
Federico Giraldi <fgiraldi@gmail.com>
2025-01-27 16:10:56 -0300

    Add new dependencies. Fix gitignore

--- generate_reviews_with_huggingface_dataset.py
+++ generate_reviews_with_huggingface_dataset.py
@@ -1,26 +1,27 @@
 import json
+import random
 from datetime import datetime
 
 from datasets import load_dataset
 
+categories = ['user experience', 'response time', 'help desk', 'accessibility', 'recommendations', 'integrations']
+
 
 # Example custom class
 class Review:
-    def __init__(self, package_name, review, date, star, products):
-        self.package_name = package_name
+    def __init__(self, review, date, star):
+        self.category = random.choice(categories)
         self.review = review
         self.date = date  # Assume this is a datetime object
         self.star = star
-        self.products = products
 
     def to_dict(self):
         """Convert the object to a serializable dictionary."""
         return {
-            "package_name": self.package_name,
+            "category": self.category,
             "review": self.review,
             "date": self.date.isoformat() if isinstance(self.date, datetime) else self.date,
-            "star": self.star,
-            "products": self.products,
+            "star": self.star
         }
 
 
@@ -30,18 +31,16 @@ output_file = "reviews.json"
 # Convert iterable to a list of serializable dictionaries
 serializable_data = [
     Review(
-        review["package_name"],
         review["review"],
         review["date"],
-        review["star"],
-        review["products"]
+        review["star"]
     ).to_dict()
     for review in dataset["test"]
 ]
 
 try:
     with open(output_file, "w", encoding="utf-8") as file:
-        json.dump(serializable_data[0:1000], file, ensure_ascii=False, indent=4)
+        json.dump(serializable_data[0:10], file, ensure_ascii=False, indent=4)
     print(f"Data successfully saved to {output_file}")
 except Exception as e:
     print(f"An error occurred: {e}")
